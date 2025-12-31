292a78377c59b69e7d86db16a3352c23deb60156
JaMaD&D <jameswaitingontheday@gmail.com>
2026-01-01 02:20:00 +0800

    applied default text color for ThemedText component

--- src/components/text/ThemedText.tsx
+++ src/components/text/ThemedText.tsx
@@ -2,6 +2,8 @@ import type { FC } from 'react';
 import type { ThemedTextProps } from '../../types/text';
 import Text from './Text';
 
-const ThemedText: FC<ThemedTextProps> = (props) => <Text {...props} />;
+const ThemedText: FC<ThemedTextProps> = (props) => (
+  <Text color={'text'} {...props} />
+);
 
 export default ThemedText;
