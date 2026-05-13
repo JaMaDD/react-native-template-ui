fc5663083bae881164c4a6ae6173e6052df565ca
JaMaD&D <jameswaitingontheday@gmail.com>
2026-05-14 01:29:22 +0800

    fixed incorrect type usage in accordion types file

--- src/types/accordion.ts
+++ src/types/accordion.ts
@@ -1,10 +1,13 @@
-import type { AnimatedThemedViewProps } from '../../lib/typescript/src';
 import type { BorderSize } from '../utils/theme/const';
 import type { ThemedPressableProps } from './button';
 import type { CustomThemedIconProps } from './icon';
 import type { CustomThemedTextProps } from './text';
 import type { ThemeColors } from './theme';
-import type { PropsWithRequiredChildren, ThemedViewProps } from './view';
+import type {
+  AnimatedThemedViewProps,
+  PropsWithRequiredChildren,
+  ThemedViewProps,
+} from './view';
 
 export type ThemedAccordionProps = PropsWithRequiredChildren<
   Partial<CustomThemedTextProps> &
